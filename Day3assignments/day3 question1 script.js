71f124fdca59ad04cd204d4e21aa64161a6682e4
venu gopal alapati <63544888+venu2000@users.noreply.github.com>
2020-07-17 14:01:02 +0530

    Add files via upload

---
+++ Day3assignments/day3 question1 script.js
@@ -0,0 +1,11 @@
+var num=prompt("enter the number");
+console.log(num);
+if(num %2 ==0)
+{
+    console.log("the number entered is "+num+" and number is even");
+
+}
+else
+{
+    console.log("the number entered is"+num+"and number is odd");
+}
---
+++ Day3assignments/day3 question3 script.js
@@ -0,0 +1,18 @@
+var num=prompt("enter the scored marks for grade");
+//var grade;
+if(num>=90){
+    console.log("marks are" +num+ "and gare is A");
+}
+else if(num>=80){
+    console.log("marks are" +num+ "and gare is B");
+}
+else if(num>=70){
+    console.log("marks are"  +num+   "and gare is C");
+}
+else{
+    console.log("marks are" +num+ "and fail");
+}
+
+
+ 
+//(num>90)?(garde='pass',console.log("congrats")):(grade='fail',console.log("try again"));
